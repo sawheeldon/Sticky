0c4626d49a4ccb0575f35aab013037766146e2e5
Simon <simon@jampot.design>
2016-10-13 23:45:57 +0000

    building out note functionality, cannot get the edit and add to work

--- build/sticky.1.0.0.js
+++ build/sticky.1.0.0.js
@@ -56,21 +56,43 @@
 	    getInitialState: function getInitialState() {
 	        return { editing: false };
 	    },
+	    componentWillMount: function componentWillMount() {
+	        this.style = {
+	            right: this.randomBetween(0, window.innerWidth - 200) + 'px',
+	            top: this.randomBetween(0, window.innerHeight - 200) + 'px',
+	            transform: 'rotate(' + this.randomBetween(-15, 15) + 'deg)'
+	        };
+	    },
+	    randomBetween: function randomBetween(min, max) {
+	        return min + Math.ceil(Math.random() * max);
+	    },
 	    edit: function edit() {
 	        this.setState({ editing: true });
 	    },
 	    save: function save() {
+	        this.props.onChange(this.refs.newText.ReactDOMgetDOMNode().value, this.props.index);
 	        this.setState({ editing: false });
 	    },
 	    remove: function remove() {
-	        alert('remove note');
+	        this.props.onRemove(this.props.index);
 	    },
 	    renderDisplay: function renderDisplay() {
 	        return React.createElement(
 	            'div',
-	            { className: 'note' },
-	            React.createElement('button', { onClick: this.edit, className: 'btn btn-success glyphicon glyphicon-pencil' }),
-	            React.createElement('button', { onClick: this.remove, className: 'btn btn-danger glyphicon glyphicon-remove' })
+	            { className: 'note', style: this.style },
+	            React.createElement(
+	                'p',
+	                null,
+	                this.props.children
+	            ),
+	            React.createElement(
+	                'span',
+	                null,
+	                React.createElement('button', { onClick: this.edit,
+	                    className: 'btn btn-success glyphicon glyphicon-pencil' }),
+	                React.createElement('button', { onClick: this.remove,
+	                    className: 'btn btn-danger glyphicon glyphicon-remove' })
+	            )
 	        );
 	    },
 	    renderForm: function renderForm() {
@@ -79,7 +101,8 @@
 	            { className: 'note' },
 	            React.createElement(
 	                'textarea',
-	                { className: 'form-control' },
+	                { ref: 'newText', value: this.props.children,
+	                    className: 'form-control' },
 	                ' '
 	            ),
 	            React.createElement('button', { onClick: this.save, className: ' btn btn-primary glyphicon glyphicon-ok' })
@@ -94,8 +117,69 @@
 	    }
 	});
 	
+	var Board = React.createClass({
+	    displayName: 'Board',
+	
+	    propTypes: {
+	        count: function count(props, propName) {
+	            if (typeof props[propName] !== "number") {
+	                return new Error('The count property must be a number');
+	            }
+	            if (props[propName] > 100) {
+	                return new Error("Creating " + props[propName] + " notes is stupid");
+	            }
+	        }
+	    },
+	    getInitialState: function getInitialState() {
+	        return {
+	            notes: []
+	        };
+	    },
+	    nextId: function nextId() {
+	        this.uniqueId = this.uniqueId || 0;
+	        return this.uniqueId++;
+	    },
+	    add: function add(text) {
+	        var arr = this.state.notes;
+	        arr.push({
+	            id: this.nextId(),
+	            note: text
+	        });
+	        this.setState({ notes: arr });
+	    },
+	    update: function update(newText, i) {
+	        var arr = this.state.notes;
+	        arr[i].note = newText;
+	        this.setState({ notes: arr });
+	    },
+	    remove: function remove(i) {
+	        var arr = this.state.notes;
+	        arr.splice(i, 1);
+	        this.setState({ notes: arr });
+	    },
+	    everyNote: function everyNote(note, i) {
+	        return React.createElement(
+	            Note,
+	            { key: note.id,
+	                index: i,
+	                onChange: this.update,
+	                onRemove: this.remove
+	            },
+	            note.note
+	        );
+	    },
+	    render: function render() {
+	        return React.createElement(
+	            'div',
+	            { className: 'board' },
+	            this.state.notes.map(this.everyNote),
+	            React.createElement('button', { className: 'btn btn-lg btn-warning glyphicon glyphicon-plus', onClick: this.add.bind(null, "Edit Me") })
+	        );
+	    }
+	});
+	
 	document.addEventListener('DOMContentLoaded', function () {
-	    ReactDOM.render(React.createElement(Note, null), document.getElementById('test'));
+	    ReactDOM.render(React.createElement(Board, { count: 10 }), document.getElementById('test'));
 	});
 
 /***/ },
--- index.js
+++ index.js
@@ -5,22 +5,38 @@ var ReactDOM = require('react-dom');
 
 var Note = React.createClass({
     getInitialState:function(){
-      return {editing: false}  
+      return {editing: false};  
+    },
+    componentWillMount: function() {
+        this.style = {
+            right: this.randomBetween(0, window.innerWidth - 200) + 'px',
+            top: this.randomBetween(0, window.innerHeight - 200) + 'px',
+            transform: 'rotate(' + this.randomBetween(-15, 15) + 'deg)'
+        };
+    },
+    randomBetween: function(min, max) {
+        return (min + Math.ceil(Math.random() * max));
     },
     edit: function() {
-      this.setState({editing:true})
+      this.setState({editing:true});
     },
     save: function(){
-      this.setState({editing:false})
+        this.props.onChange(this.refs.newText.ReactDOMgetDOMNode().value, this.props.index);
+        this.setState({editing:false});
     },
     remove: function(){
-      alert('remove note');  
+     this.props.onRemove(this.props.index);
     },
     renderDisplay: function(){
         return(
-            <div className="note">
-            <button onClick={this.edit} className="btn btn-success glyphicon glyphicon-pencil"/>
-            <button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-remove"/>
+            <div className="note" style={this.style}>
+                <p>{this.props.children}</p>
+                <span>
+                <button onClick={this.edit} 
+                    className="btn btn-success glyphicon glyphicon-pencil"/>
+                <button onClick={this.remove} 
+                    className="btn btn-danger glyphicon glyphicon-remove"/>
+                </span>    
             </div>
             
             );
@@ -28,7 +44,8 @@ var Note = React.createClass({
     renderForm: function(){
         return(
             <div className="note">
-            <textarea className="form-control"> </textarea>
+            <textarea ref="newText" value={this.props.children} 
+                className="form-control"> </textarea>
                 <button onClick={this.save} className=" btn btn-primary glyphicon glyphicon-ok" />
             </div>
             );
@@ -45,6 +62,65 @@ var Note = React.createClass({
 
 
 
+var Board = React.createClass({
+      propTypes: {
+        count: function(props, propName) {
+            if (typeof props[propName] !== "number"){
+                return new Error('The count property must be a number');
+            }
+            if (props[propName] > 100) {
+                return new Error("Creating " + props[propName] + " notes is stupid");
+            }
+        }
+    },
+    getInitialState: function() {
+        return {
+            notes: [
+               
+            ]
+        };
+    },
+    nextId: function() {
+        this.uniqueId = this.uniqueId || 0;
+        return this.uniqueId++;
+    },
+    add: function(text){
+        var arr = this.state.notes;
+        arr.push({
+            id: this.nextId(),
+            note: text
+        });
+        this.setState({notes: arr});
+    },
+    update: function(newText, i) {
+        var arr = this.state.notes;
+        arr[i].note = newText;
+        this.setState({notes:arr});
+    },
+    remove: function(i) {
+        var arr = this.state.notes;
+        arr.splice(i, 1);
+        this.setState({notes: arr});
+    },
+    everyNote: function(note, i) {
+        return (
+                <Note key={note.id}
+                    index={i}
+                    onChange={this.update}
+                    onRemove={this.remove}
+                >{note.note}</Note>
+            );
+    },
+    render: function() {
+        return (<div className="board">
+                    {this.state.notes.map(this.everyNote)}
+                    <button className="btn btn-lg btn-warning glyphicon glyphicon-plus" onClick={this.add.bind(null, "Edit Me")}/>
+            </div>
+
+        );
+    }
+});
+
 
 
 
@@ -52,5 +128,5 @@ var Note = React.createClass({
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    ReactDOM.render(<Note />, document.getElementById('test'));
+    ReactDOM.render(<Board count={10} />, document.getElementById('test'));
 });
